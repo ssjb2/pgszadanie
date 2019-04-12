a44c4c89a5b019ef129defc5f68b5e2d15660d31
ssjb2 <unova4@gmail.com>
2019-04-12 22:46:51 +0200

    react build

---
+++ react-app/build/precache-manifest.4934dced3894ccb16125a476d4167828.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "47b8dbf95a77bab6cb0d",
+    "url": "/static/js/main.408637cf.chunk.js"
+  },
+  {
+    "revision": "ebca3ed692f8136d09f3",
+    "url": "/static/js/2.e129f8c0.chunk.js"
+  },
+  {
+    "revision": "47b8dbf95a77bab6cb0d",
+    "url": "/static/css/main.1f595009.chunk.css"
+  },
+  {
+    "revision": "ebca3ed692f8136d09f3",
+    "url": "/static/css/2.9cd9e869.chunk.css"
+  },
+  {
+    "revision": "8dbebb634635533220ddfcdbf9cddcc2",
+    "url": "/index.html"
+  }
+];
